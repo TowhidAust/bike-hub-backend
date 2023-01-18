5b65c26052f1fa32b5c3dd61e64ee367f0799041
TowhidAust <007towhid2016@gmail.com>
2023-01-18 12:23:04 +0600

    Initial signup test case completed

--- properties.js
+++ properties.js
@@ -1,4 +1,5 @@
 module.exports = {
-    // DBURL: "mongodb://localhost:27017/BikeHub",
-    DBURL: "mongodb+srv://TowhidAust:towhid123@cluster0.hyei6.mongodb.net/BikeHub?retryWrites=true&w=majority",
-}
+  // DBURL: "mongodb://localhost:27017/BikeHub",
+  DBURL:
+    "mongodb+srv://TowhidAust:towhid123@cluster0.hyei6.mongodb.net/bikehub?retryWrites=true&w=majority",
+};
--- src/routes/auth/controller.js
+++ src/routes/auth/controller.js
@@ -1,22 +1,20 @@
-
 const jwt = require("jsonwebtoken");
 const Signup = require("../../databse/auth/schema");
 
 const generateJwtAndSaveUsersData = async (usersData, res) => {
-    // save into db in users collection
-    const signup = new Signup(usersData);
-    await signup.save();
-    
+  console.log("called...");
+  // save into db in users collection
+  const signup = new Signup(usersData);
+  await signup.save();
 
-    
-    jwt.sign({usersData}, 'secretkey', (err, token)=>{
-        return res.json({
-            message: "user signup success",
-            token: token
-        })
-    })
-}
+  jwt.sign({ usersData }, "secretkey", (err, token) => {
+    return res.json({
+      message: "user signup success",
+      token: token,
+    });
+  });
+};
 
-module.exports ={
-    generateJwtAndSaveUsersData
-}
+module.exports = {
+  generateJwtAndSaveUsersData,
+};
