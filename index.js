24afecf41bccbf266e7e74c5783f99ecc75685eb
TowhidAust <towhidaustcse33@gmail.com>
2023-02-10 18:20:41 +0600

    cors issue fix and login issue fix

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 const path = require('path');
+var cors = require('cors');
+
+
+var allowlist = ['http://localhost:3005', 'http://example2.com']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.use(cors(corsOptionsDelegate))
 
 /**
   *setup .env file paths for dev, staging, prod
--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -7,13 +7,15 @@ const { decryption, generateResponse, encryption } = require("../../helper");
 
 router.post('/', async (req, res) => {
     const { phone, password } = req.body;
-    
+    console.log(phone, password);
     Signup.findOne({ phone: phone }, (error, data) => {
+        console.log(error, data);
         if (error) {
             res.status(500);
             return res.json(500, error?.message);
         }
-        if (password === decryption(data?.password)) {
+   
+        if (data && password === decryption(data?.password)) {
             jwt.sign({ firstname: data?.firstname, lastname: data?.lastname, id: data?._id }, 'secretkey', (err, token) => {
                 if (err) {
                     res.status(500);
